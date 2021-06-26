a7397de76adf8b49c1e03175d596d9d7f2d8da8e
Natan Felles <natanfelles@gmail.com>
2021-06-26 11:02:29 -0300

    Update Coding Standard - add header comments

--- tests/UserAgentTest.php
+++ tests/UserAgentTest.php
@@ -1,4 +1,13 @@
-<?php namespace Tests\HTTP;
+<?php
+/*
+ * This file is part of The Framework HTTP Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\HTTP;
 
 use Framework\HTTP\UserAgent;
 use PHPUnit\Framework\TestCase;
