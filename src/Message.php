b7bed32cd579e86164cf2c87a39d41fcff529629
Natan Felles <natanfelles@gmail.com>
2021-09-09 01:53:40 -0300

    Add more headers that appends with a new line

--- src/Message.php
+++ src/Message.php
@@ -390,9 +390,16 @@ abstract class Message implements MessageInterface
     {
         $headerName = \strtolower($headerName);
         if (\in_array($headerName, \array_map('strtolower', [
+            MessageInterface::HEADER_DATE,
+            RequestInterface::HEADER_IF_MODIFIED_SINCE,
+            RequestInterface::HEADER_IF_RANGE,
+            RequestInterface::HEADER_IF_UNMODIFIED_SINCE,
+            ResponseInterface::HEADER_EXPIRES,
+            ResponseInterface::HEADER_LAST_MODIFIED,
+            ResponseInterface::HEADER_PROXY_AUTHENTICATE,
+            ResponseInterface::HEADER_RETRY_AFTER,
             ResponseInterface::HEADER_SET_COOKIE,
             ResponseInterface::HEADER_WWW_AUTHENTICATE,
-            ResponseInterface::HEADER_PROXY_AUTHENTICATE,
         ]), true)) {
             return "\n";
         }
