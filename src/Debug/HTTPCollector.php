238b9d2b20846d32dfe86415919a0a75153aa086
Natan Felles <natanfelles@gmail.com>
2026-01-03 01:41:12 -0300

    Update class name

--- src/Debug/HTTPCollector.php
+++ src/Debug/HTTPCollector.php
@@ -79,7 +79,7 @@ class HTTPCollector extends Collector
         \ob_start(); ?>
         <p>
             <strong>IP:</strong> <?= $this->request->getIp() ?>
-            <span class="opaque">(<?= $this->request->getIpKey() ?>)</span>
+            <span class="text-opaque">(<?= $this->request->getIpKey() ?>)</span>
         </p>
         <p><strong>Is Secure:</strong> <?= $this->request->isSecure() ? 'Yes' : 'No' ?></p>
         <p><strong>Protocol:</strong> <?= $this->request->getProtocol() ?></p>
